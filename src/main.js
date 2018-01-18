d79dfe420fe1fa01f6bb57f85869f3276f72b246
osadnriets@gmail.com <gjx2890-12345>
2018-01-19 00:41:26 +0200

    Before 81

--- src/main.js
+++ src/main.js
@@ -9,18 +9,53 @@ new Vue({
     // data:{
     //      msg: 'Hello world'
     // },
+    data: {
+      genre: [],
+      time: []
+    },
+    methods: {
+        checkFilter(category, title, checked){
+            console.log(category, title, checked);
+            // this.$emit('check-filter');
+            if(checked){
+                this[category].push(title);
+            } else {
+                let index = this[category].indexOf(title);
+                if(index > -1){
+                    this[category].splice(index,1);
+                }
+            }
+        }
+    },
     components: {
          'movie-list': {
              template: '<div id="movie-list">' +
-             '<div v-for="movie in movies" class="movie">{{movie.title}}</div>' +
+             '<div v-for="movie in filteredModules" class="movie">{{movie.title}}</div>' +
              '</div>',
              data(){
                  return {
                      movies: [
-                         {title: 'Pulp Fiction'},
-                         {title: 'Home Along'},
-                         {title: 'Austin Powers'},
+                         {title: 'Pulp Fiction', genre: genres.CRIME},
+                         {title: 'Home Along', genre: genres.COMEDY},
+                         {title: 'Austin Powers', genre: genres.COMEDY}
                      ]
+                 }
+             },
+             props: ['genre', 'time'],
+             methods: {
+                 moviePassesGenreFilter(movie){
+                     if(!this.genre.length){
+                         return true;
+                     }
+                     else {
+                         return this.genre.find(genre => movie.genre === genre);
+                     }
+                 }
+             },
+             computed: {
+                 filteredModules(){
+                     //return this.movies;
+                     return this.movies.filter(this.moviePassesGenreFilter);
                  }
              }
          },
@@ -30,17 +65,36 @@ new Vue({
                    genres
                }
              },
-            template: '<div id="movie-filter">' +
-            '<h2>Filter result</h2>' +
-                '<div class="filter-group">' +
-                '{{genre}}' +
-                '<check-filter v-for="genre in genres" v-bind:title="genre" ></check-filter>' +
-                '</div>' +
-            '</div>',
+            template: `<div id="movie-filter">
+                            <h2>Filter result</h2>
+                            <div class="filter-group">
+                                <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+                            </div>
+                        </div>`,
+            methods: {
+                checkFilter(category, title, checked){
+                    //console.log('checkFilter');
+                    this.$emit('check-filter', category, title, checked);
+
+                }
+            },
             components: {
                 'check-filter': {
+                    data(){
+                        return{
+                            checked: false
+                        }
+                    },
                     props: ['title'],
-                    template: '<div>{{title}}</div>'
+                    template: `<div  class="check-filter" v-bind:class="{active: checked}" v-on:click="checkFilter">
+                    <span class="checkbox"></span><span class="check-filter-title">{{title}}</span>
+                    </div>`,
+                    methods: {
+                        checkFilter(){
+                            this.checked = !this.checked;
+                            this.$emit('check-filter', 'genre', this.title, this.checked);
+                        }
+                    }
                 }
             }
         }
