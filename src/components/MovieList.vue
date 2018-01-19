41102f83bf2b05723766dea99f3c1578d348da3f
osadnriets@gmail.com <gjx2890-12345>
2018-01-19 23:27:09 +0200

    Before 97

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,13 +1,23 @@
 <template>
     <div id="movie-list">
-        <div v-for="movie in filteredModules" class="movie">{{movie.title}}</div>
+        <!--<div v-for="item in filteredModules" class="movie">{{item.movie.Title}}</div>-->
+        <div v-if="filteredModules.length">
+            <movie-item v-for="movie in filteredModules" v-bind:movie="movie.movie"></movie-item>
+        </div>
+        <div v-else-if="movies.length" class="no-results">
+            No result...
+        </div>
+        <div v-else class="no-results">
+            Loading...
+        </div>
     </div>
 </template>
 <script>
     import genres from '../util/genres';
+    import MovieItem from './MovieItem.vue'
 
     export  default {
-        data(){
+/*        data(){
             return {
                 movies: [
                     {title: 'Pulp Fiction', genre: genres.CRIME},
@@ -15,15 +25,23 @@
                     {title: 'Austin Powers', genre: genres.COMEDY}
                 ]
             }
-        },
-        props: ['genre', 'time'],
+        },*/
+        props: ['genre', 'time', 'movies'],
         methods: {
             moviePassesGenreFilter(movie){
                 if(!this.genre.length){
                     return true;
                 }
                 else {
-                    return this.genre.find(genre => movie.genre === genre);
+                    //return this.genre.find(genre => movie.genre === genre);
+                    let movieGenres = movie.movie.Genre.split(", ");
+                    let matched = true;
+                    this.genre.forEach(genre => {
+                       if (movieGenres.indexOf(genre) === -1){
+                           matched = false;
+                       }
+                    });
+                    return matched;
                 }
             }
         },
@@ -32,6 +50,9 @@
                 //return this.movies;
                 return this.movies.filter(this.moviePassesGenreFilter);
             }
+        },
+        components: {
+            MovieItem
         }
     }
 </script>
